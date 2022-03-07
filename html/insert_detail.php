61b9bc2270d75e3391c6733931d8a96c8b9ff60a
20jy0137 <20jy0137@jec.ac.jp>
2022-03-07 15:51:53 +0900

    commit

---
+++ html/insert_detail.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    $dsn = 'sqlsrv:server=10.42.129.3;database=20grb1';
+    $user = '20grb1';
+    $password = '20grb1';
+    //PDOオブジェクトの作成
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    print "接続エラー!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+
+?>
