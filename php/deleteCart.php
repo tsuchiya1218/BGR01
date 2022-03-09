bd0a88d3cd96f9356d77cc9bfa8820a9fdeccb86
20jy0141 <20jy0141@jec.ac.jp>
2022-03-09 18:37:01 +0900

    commit yokoi

--- php/deleteCart.php
+++ php/deleteCart.php
@@ -1,30 +1,29 @@
 <?php
-//データベースに接続する
-try {
-    $server_name = "10.42.129.3";    // サーバ名
-    $db_name = "20grb1";    // データベース名(自分の学籍番号を入力)
+    //データベースに接続する
+    try {
+        $server_name = "10.42.129.3";	// サーバ名
+        $db_name = "20grb1";	// データベース名(自分の学籍番号を入力)
 
-    $user_name = "20grb1";    // ユーザ名(自分の学籍番号を入力)
-    $user_pass = "20grb1";    // パスワード(自分の学籍番号を入力)
+        $user_name = "20grb1";	// ユーザ名(自分の学籍番号を入力)
+        $user_pass = "20grb1";	// パスワード(自分の学籍番号を入力)
 
-    // データソース名設定
-    $dsn = "sqlsrv:server=$server_name;database=$db_name";
+        // データソース名設定
+        $dsn = "sqlsrv:server=$server_name;database=$db_name";
 
-    // PDOオブジェクトのインスタンス作成
-    $pdo = new PDO($dsn, $user_name, $user_pass);
+        // PDOオブジェクトのインスタンス作成
+        $pdo = new PDO ($dsn, $user_name, $user_pass);
 
-    // PDOオブジェクトの属性の指定
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    print "接続エラー!: " . $e->getMessage();
-    exit();
-}
-
-
+        // PDOオブジェクトの属性の指定
+        $pdo ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    } catch ( PDOException $e ) {
+        print "接続エラー!: " . $e->getMessage ();
+        exit();
+    }
 
 $b_code = $_GET['b_code'];
 $c_code = $_GET['c_code'];
 $b = $_GET['b'];
+
     //buycart
     if($b == 'buycart'){
         try{
