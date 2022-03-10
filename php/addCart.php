41b0aa0228e8f1312b7d7169ca213a7a4e0b1fce
20jy0137 <20jy0137@jec.ac.jp>
2022-03-10 16:55:02 +0900

    commit main

--- php/addCart.php
+++ php/addCart.php
@@ -24,99 +24,131 @@ try {
 // $cart = $_SESSION['cart'];
 // $c_code = $_SESSION['c_code'];
 
-$c_code = $_SESSION['c_code']; //セッション
+$c_code = $_SESSION['c_code']; //セッションから顧客コード
 $b_code = $_GET['b_code']; //本のコード パラメータ
 $cart = $_GET['cart']; //カート種別　hidden
 $price = $_GET['price']; //本の値段 hidden
 
+//echo $c_code. $b_code. $cart.$price;
+
+
+//カートに商品がすでに入ってるかどうかを確認
+//$testsql = "SELECT COUNT(*) as co FROM buycart WHERE c_code = ? AND b_code ?";
+
 
 //購入カート
 if ($cart == 'buycart') {
+
     //buycart表からbc_codeをカウント
-    $sql = "SELECT COUNT(bc_buyCartCode) as bc_count FROM buycart";
-    try {
+    $testsql = "SELECT COUNT(*) as co FROM buycart WHERE c_code = '" . $c_code . "' AND b_code ='" . $b_code . "'";
+    $stmt = $pdo->prepare($testsql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_BOTH);
 
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $arraycount = $stmt->fetch(PDO::FETCH_BOTH);
-        $sql = null;
-        $stmt = null;
-    } catch (PDOException $e) {
-        print "SQL 実行エラー!: " . $e->getMessage();
-        exit();
-    }
-    $count = $arraycount[0]['bc_count'] + 1;
+    if ($row['co'] != 0) {
+        $_SESSION["eMsg"] = "既にカートに入っています。";
+        // header("Location: $_SESSION);
+    } else {
 
-    $sql = "INSERT INTO buycart(c_code,bc_buyCartCode,b_code,bc_qty,bc_totalamount)
+        $sql = "SELECT MAX(bc_buyCartCode) as bc_count FROM buycart";
+        try {
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $count = $stmt->fetch(PDO::FETCH_BOTH);
+        } catch (PDOException $e) {
+            print "SQL 実行エラー!: " . $e->getMessage();
+            exit();
+        }
+        $count[0] += 1;
+
+        $sql = "INSERT INTO buycart(c_code,bc_buyCartCode,b_code,bc_qty,bc_totalamount)
                             VALUES(?,?,?,1,?)";
-    try {
-        $stmt = $pdo->prepare($sql);
-        //SQL実行
-        $stmt->execute(array($c_code, $count, $b_code, $price));
-        $sql = null;
-        $stmt = null;
-    } catch (PDOException $e) {
-        print "SQL 実行エラー!: " . $e->getMessage();
-        exit();
+        try {
+            $stmt = $pdo->prepare($sql);
+            //SQL実行
+            $stmt->execute(array($c_code, $count[0], $b_code, $price));
+            $sql = null;
+            $stmt = null;
+        } catch (PDOException $e) {
+            print "SQL 実行エラー!: " . $e->getMessage();
+            exit();
+        }
     }
-
     //予約カート
 } else if ($cart == 'reservecart') {
 
     //reservecart表からbc_codeをカウント
-    $sql = "SELECT COUNT(rc_reserveCartCode) as rc_count FROM reservecart";
-    try {
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $arraycount = $stmt->fetch(PDO::FETCH_BOTH);
-        $sql = null;
-        $stmt = null;
-    } catch (PDOException $e) {
-        print "SQL 実行エラー!: " . $e->getMessage();
-        exit();
-    }
-    $count = $arraycount[0]['rc_count'] + 1;
+    $testsql = "SELECT COUNT(*) as co FROM reservecart WHERE c_code = '" . $c_code . "' AND b_code ='" . $b_code . "'";
+    $stmt = $pdo->prepare($testsql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_BOTH);
 
-    $sql = "INSERT INTO reservecart(c_code,rc_reserveCartCode,b_code,rc_qty,rc_totalamount)
+    if ($row['co'] != 0) {
+        $_SESSION["eMsg"] = "既にカートに入っています。";
+        // header("Location: $_SESSION);
+    } else {
+
+        $sql = "SELECT MAX(bc_buyCartCode) as bc_count FROM reservecart";
+        try {
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $count = $stmt->fetch(PDO::FETCH_BOTH);
+        } catch (PDOException $e) {
+            print "SQL 実行エラー!: " . $e->getMessage();
+            exit();
+        }
+        $count[0] += 1;
+
+        $sql = "INSERT INTO buycart(c_code,bc_buyCartCode,b_code,bc_qty,bc_totalamount)
                             VALUES(?,?,?,1,?)";
-    try {
-        $stmt = $pdo->prepare($sql);
-        //SQL実行
-        $stmt->execute(array($c_code, $count, $b_code, $price));
-        $sql = null;
-        $stmt = null;
-    } catch (PDOException $e) {
-        print "SQL 実行エラー!: " . $e->getMessage();
-        exit();
+        try {
+            $stmt = $pdo->prepare($sql);
+            //SQL実行
+            $stmt->execute(array($c_code, $count[0], $b_code, $price));
+            $sql = null;
+            $stmt = null;
+        } catch (PDOException $e) {
+            print "SQL 実行エラー!: " . $e->getMessage();
+            exit();
+        }
     }
-
     //レンタルカート
 } else if ($cart == 'rentalcart') {
-    //rentalcart表からbc_codeをカウント
-    $sql = "SELECT COUNT(rtc_code) as rtc_count FROM rentalcart";
-    try {
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $arraycount = $stmt->fetch(PDO::FETCH_BOTH);
-        $sql = null;
-        $stmt = null;
-    } catch (PDOException $e) {
-        print "SQL 実行エラー!: " . $e->getMessage();
-        exit();
-    }
-    $count = $arraycount[0]['rtc_count'] + 1;
+    $testsql = "SELECT COUNT(*) as co FROM rentalcart WHERE c_code = '" . $c_code . "' AND b_code ='" . $b_code . "'";
+    $stmt = $pdo->prepare($testsql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_BOTH);
 
-    $sql = "INSERT INTO rentalcart(c_code,rtc_code,b_code,rtc_totalamount)
-                            VALUES(?,?,1,?)";
-    try {
-        $stmt = $pdo->prepare($sql);
-        //SQL実行
-        $stmt->execute(array($c_code, $count, $b_code, $price));
-        $sql = null;
-        $stmt = null;
-    } catch (PDOException $e) {
-        print "SQL 実行エラー!: " . $e->getMessage();
-        exit();
+    if ($row['co'] != 0) {
+        $_SESSION["eMsg"] = "既にカートに入っています。";
+        // header("Location: $_SESSION);
+    } else {
+        //rentalcart表からbc_codeをカウント
+        $sql = "SELECT COUNT(rtc_code) as bc_count FROM rentalcart";
+        try {
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $arraycount = $stmt->fetch(PDO::FETCH_BOTH);
+            $sql = null;
+            $stmt = null;
+        } catch (PDOException $e) {
+            print "SQL 実行エラー!: " . $e->getMessage();
+            exit();
+        }
+        $count = $arraycount[0]['bc_count'] + 1;
+
+        $sql = "INSERT INTO buycart(c_code,rtc_code,b_code,bc_qty,bc_totalamount)
+                            VALUES(?,?,?,1,?)";
+        try {
+            $stmt = $pdo->prepare($sql);
+            //SQL実行
+            $stmt->execute(array($c_code, $count, $b_code, $price));
+            $sql = null;
+            $stmt = null;
+        } catch (PDOException $e) {
+            print "SQL 実行エラー!: " . $e->getMessage();
+            exit();
+        }
     }
 }
-header('Location:buycart.php');
+header("Location:buyCart.php");
