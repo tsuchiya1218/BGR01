179dd6efa690ec5136413087d930bca32cf8cbcf
20jy0123 <20jy0123@jec.ac.jp>
2022-03-10 09:53:21 +0900

    commit

--- php/Receiving.php
+++ php/Receiving.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+$c_code = $_SESSION['c_code'];//顧客コード
+$cart = $_SESSION['cart'];//カート種別
+
+if ($cart == 'rentalcart') {
+    // payment.phpに遷移する
+    header("payment.php");
+    exit;
+}
+
 
 //データベースに接続する
 try {
@@ -22,28 +31,22 @@ try {
     exit();
 }
 
-// お客様情報
-//サンプル
-$c_code = $_GET['c_code']; //顧客コード
-var_dump($_GET['c_code']);
-$cart = $_SESSION['cart'] = 'buycart'; //カート種別
-// 購入
-$cart = array('buycart' => 0, 'reservecart' => 1, 'rentalcart' => 2);
-if ($cart == 2) {
-    // payment.phpに遷移する
-    header("payment.php");
-    exit;
-}
-$sql = "SELECT * FROM store WHERE c_code = ?";
-try {
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array($c_code));
-    $array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $stmt = null;
-} catch (PDOException $e) {
-    print "接続エラー!: " . $e->getMessage();
-    exit();
-}
+    // お客様情報
+    // 購入
+    $Cart = array('buycart' => 0, 'reservecart' => 1, 'rentalcart' => 2);
+    //サンプル $c_code = '1';
+    // $sql = "SELECT * FROM store WHERE c_code = ?";
+
+    $sql = "SELECT * FROM store WHERE c_code = ?";
+    try {
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($c_code));
+        $array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = null;
+    } catch (PDOException $e) {
+        print "接続エラー!: " . $e->getMessage();
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -85,8 +88,8 @@ try {
         <div align="center">
             <p>受取方法</p>
             <form action="Receiving_get.php" method="GET">
-                <input type="hidden" value="<?= $cart ?>">
-                <input type="hidden" value="<?= $c_code ?>">
+                <input type="hidden" value="<?= $cart?>">
+                <input type="hidden" value="<?= $c_code?>">
                 <input type="radio" name="select" value="store">店舗
                 <input type="radio" name="select" value="delivery" checked>郵送
                 <input type="submit" value="次へ">
